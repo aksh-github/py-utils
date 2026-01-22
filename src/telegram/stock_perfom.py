aff71ac84b42b4aa9390b1f9ca47199b77fefa15
Akshay B <beatle02@gmail.com>
2026-01-22 12:43:17 +0530

    Buy price, Qty incorporated and corr. perform impl

--- src/telegram/stock_perfom.py
+++ src/telegram/stock_perfom.py
@@ -9,18 +9,27 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def read_stocks():
     with open('./src/telegram/stocks.txt', 'r') as f:
         stocks = []
-
         for line in f:
             line = line.strip()
             if line == '':
                 break
-            stocks.append(line)
-            
-    return(stocks)
+            # Split by comma
+            data = line.split(',')
+            # stock = [0].strip()
+            stocks.append({
+                'stock': data[0],
+                'buy_price': float(data[1]),
+                'qty': int(data[2]),
+            })
+    return stocks
 
 def_time = '7d'
 
-def get_stock_performance(ticker, period=def_time):
+def get_stock_performance(stockObj, period=def_time):
+    ticker = stockObj["stock"]
+    buy_price = stockObj["buy_price"]
+    qty = stockObj["qty"]
+
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     
@@ -29,37 +38,42 @@ def get_stock_performance(ticker, period=def_time):
         return (f"No data found for {ticker}")
                 
     current_price = data['Close'].iloc[-1]  # today's price
-    yest_price = data['Close'].iloc[-2]     # yest's price
+    # yest_price = data['Close'].iloc[-2]     # yest's price
 
-    oldest_price =  data['Close'].iloc[0]     # price before 7 / 15 d etc.
-
-
-    # calc for 15d to today    
-    change = (current_price - oldest_price)
-    perc_change = change / oldest_price * 100
+    lastXdayPrice =  data['Close'].iloc[0]     # price before 7 / 15 d etc.
 
 
-    lastchange = (current_price - yest_price)
-    last_perc_change = lastchange / yest_price * 100
+    # calc for buy price to today    
+    change = (current_price - buy_price)
+    perc_change = change / buy_price * 100
+
+
+    # calc for last x'th day to today    
+    recent_chng = (current_price - lastXdayPrice)
+    recent_perc_chng = recent_chng / lastXdayPrice * 100
     
     # print(f"Stock: {ticker}")
     # print(f"Current Price: ₹{current_price:.2f}")
-    # print(f"Open Price: ₹{oldest_price:.2f}")
+    # print(f"Open Price: ₹{lastXdayPrice:.2f}")
     # print(f"Change: {change:.2f} ({perc_change:.2f}%)")
     logging.info(f"Got data for {ticker}")
 
-    # return(f"**{ticker}** : \n\r({oldest_price:.2f} to {current_price:.2f}) {change:.2f} ({perc_change:.2f}%)")
+    # return f"""**{ticker}: {buy_price} , Qty: {qty}** : 
+    # ({lastXdayPrice:.2f} to {current_price:.2f}) {change:.2f} ({perc_change:.2f}%)
+    # (Since yest: {yest_price:.2f} to {current_price:.2f}) {recent_chng:.2f} ({recent_perc_chng:.2f}%)"""
 
-    return f"""**{ticker}** : 
-    ({oldest_price:.2f} to {current_price:.2f}) {change:.2f} ({perc_change:.2f}%)
-    (Since yest: {yest_price:.2f} to {current_price:.2f}) {lastchange:.2f} ({last_perc_change:.2f}%)"""
+    return f"""**{ticker}: {buy_price} , Qty: {qty}** : 
+    (Total Change: {buy_price:.2f} to {current_price:.2f}) {change:.2f} ({perc_change:.2f}%)
+    (Since last x days: {lastXdayPrice:.2f} to {current_price:.2f}) {recent_chng:.2f} ({recent_perc_chng:.2f}%)"""
     
 
 def process(period):
     stocks = read_stocks()
 
     resp = ""
-    for s in stocks:
-        resp = resp + get_stock_performance(s, period=period) + "\n\r" + "\n\r"
+    # for s in stocks:
+    #     resp = resp + get_stock_performance(s, period=period) + "\n\r" + "\n\r"
+    for stock in stocks:
+        resp = resp + get_stock_performance(stock, period=period) + "\n\r" + "\n\r"
 
     return resp
